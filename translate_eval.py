2bf68263fcb799cc21422c8b7f1433ad5a6dc39e
impassionate <46687271+impassionate@users.noreply.github.com>
2023-11-04 12:11:02 -0500

    Update translate_eval.py

--- translate_eval.py
+++ translate_eval.py
@@ -109,6 +109,6 @@ def extract_translate_and_evaluate(java_file_path, api_key):
     return results
 
 # usage:
-api_key = "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"  # Remember to use your own key
+api_key = "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"  # Remember to use your own key
 results = extract_translate_and_evaluate("test.docx", api_key)
 #print(results)
